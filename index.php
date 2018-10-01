430739cd8142b6daf78fb2fc81248dcb2f47f304
Mark Niel Magtibay <43737953+marknielmagtibay@users.noreply.github.com>
2018-10-01 13:22:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,225 @@
-<!HTML5>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  </head>
+
+  <body>
+
+    <div id = "main-container">
+      <section id = "home" class = "container center-text">
+
+        <h1 class = "full-name">I'm Mark Niel Magtibay.</h1>
+        <p>Web Developer | Shopify and WordPress Specialist</p>
+        <img class = "h-image" src = "https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/Mark-Niel-Magtibay-1.png">
+      </section>
+
+      <section id = "about-me" class = "container">
+        <h2>" C H A P S "</h2>
+        <hr style="width:200px" class="w3-opacity">
+          <div class = "about-me-wrapper">
+            <div class="roles">
+                <div>
+                    I am a front-end <span style = "color:#e8b900;">web developer.<span>
+                </div>
+
+                <div>
+                    I am a <span style = "color:#e8b900;">gamer.</span>
+                </div>
+
+                <div>
+                    I am an <span style = "color:#e8b900;">aspiring individual.</span>
+                </div>
+
+              </div>
+            </div>
+            <div class = "about-me-container">
+              <i class="fas fa-laptop-code fa-5x"></i><p>Hi I'm Niel A.K.A "Chaps" <span><i class="far fa-smile-beam"></i></span>. I'm a web developer (obviously <span><i class="far fa-laugh-wink"></i></span>). Nope I'm not a hacker, nor I can't hack your boyfriend's social media accounts. <span><i class="far fa-laugh-squint"></i></i></span>
+
+                <p>I am a skilled developer with a strong focus on both WordPress and Shopify websites. Allow me to assist you in developing custom sites for your clients!</p>
+
+                <h3>Here are just some of my skills and experience:</h3>
+
+                <ul style="margin-left: 0px;padding-left: 0px;">
+                  <li>high proficiency with CSS, HTML, PHP, Javascript and other programming languages</li>
+                  <li>website development / building websites from scratch / modifications</li>
+                  <li>theme and plugin development, installation and customization</li>
+                  <li>site optimization, bug checking and fixes</li>
+                  <li>site security protection</li>
+                  <li>site migration / hosting transfer</li>
+                  <li>CSS customization</li>
+                </ul>
+
+
+              </p>
+            </div>
+
+            <div class = "about-me-container">
+              <i class="fas fa-gamepad fa-5x"></i><p>No Game No Life. <span><i class="far fa-grin-beam-sweat"></i></span> I'm not a total geek tho. Just an average gamer. Come on! who doesn't like playing console and computer games? <span><i class="far fa-grin-tears"></i></span>
+
+                <p>I usually play Tekken, League of Legends, Dota,  Dota 2, Mobile Legends, PUBG, Cross Fire, Free Fire, Rules of Survival, Fortnite, Cabal, Dragon Nest, Outlast, God of War, NFS, etc. Oh also Minecraft, like who doesn’t like Minecraft? <span><i class="fas fa-gamepad"></i></span></p>
+
+                <p>Playing games is my way of relaxing and taking a break from adulting. I've been playing arcade to computer games since I was a kid. Up until now I'm still trying to become a Saiyan like every kid does. <span><i class="far fa-grin-tears"></i></span> </p>
+
+                <p>So much games to mention. I told you, just an avarage player, maybe? <span><i class="far fa-grin-squint-tears"></i></span></p>
+              </p>
+            </div>
+
+            <div class = "about-me-container">
+              <i class="far fa-grin-squint-tears fa-5x"></i><p>I am an active person, (despite of my shape <span><i class="far fa-grin-tears"></i></span>) I like swimming, biking, hiking, travels and a party animal. What I love about myself, I can easily blend in to the people around me. I don’t like sweets and milk. I won’t drink milk even if I’m half to death of thirst. I'm a lactose intolerant in other words. <span><i class="far fa-frown"></i></span> I don’t like sweets , not a fan. I would rather eat spicy foods than sweet foods like chocolates and other stuffs.
+
+              <p>I love watching series, anime and movies. I love watching western movies and TV series. Game of Thrones ("Valar Morghulis"), Stranger Things, Fresh off the boat, etc. Oh! also I watch surgical videos. Not to creep you out. I'm a doctor. Just kidding, supposedly a doctor by profession.</p>
+
+              <p>Being in a crowd or group of friends makes me feel like there’s a need to crack jokes, to make it simple , I am the clown. A bit humorous and sometimes close to bullying. <span><i class="far fa-hand-peace"></i></span> </p>
+
+
+</p>
+            </div>
+
+        </section>
+
+      <section id = "skills" class = "container">
+        <h2>My Skills</h2>
+        <hr style="width:200px" class="w3-opacity">
+        <br>
+
+        <p class = "w3-wide">HTML / HTML5</p>
+        <div class="skillbar clearfix " data-percent="95%">
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">95%</div>
+</div>
+
+        <p class = "w3-wide">CSS / CSS3</p>
+        <div class="skillbar clearfix " data-percent="92%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">92%</div>
+</div>
+
+        <p class = "w3-wide">JavaScript</p>
+        <div class="skillbar clearfix " data-percent="80%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">80%</div>
+</div>
+
+        <p class = "w3-wide">Liquid</p>
+        <div class="skillbar clearfix " data-percent="85%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">85%</div>
+</div>
+
+        <p class = "w3-wide">MailChimp</p>
+        <div class="skillbar clearfix " data-percent="90%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">90%</div>
+</div>
+
+        <p class = "w3-wide">Shopify</p>
+        <div class="skillbar clearfix " data-percent="85%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">85%</div>
+</div>
+
+        <p class = "w3-wide">WordPress</p>
+        <div class="skillbar clearfix " data-percent="83%">
+
+<div class="skillbar-bar" style="background: #616161;"></div>
+<div class="skill-bar-percent">83%</div>
+</div>
+      </section>
+
+
+      <section id = "work" class = "container">
+        <h2>Work and Credentials</h2>
+        <hr style="width:200px" class="w3-opacity">
+        <br>
+          <div>
+            <img class="zoom-image" src="https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/The-Digital-Garage-Certification-1.jpg" alt="">
+            <img class="zoom-image" src="https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/Content-Marketing-Mark-Niel-Magtibay.png" alt="">
+            <img class="zoom-image" src="https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/Inbound-Mark-Niel-Magtibay.png" alt="">
+            <img class="zoom-image" src="https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/Email-Marketing-Mark-Niel-Magtibay.png" alt="">
+            <img class="zoom-image" src="https://usource.agency/webdev/Niel/portfolio1/wp-content/uploads/2018/09/Inbound-Marketing-Mark-Niel-Magtibay.png" alt="">
+
+          </div>
+      </section>
+
+      <section id = "lets-talk"class = "container">
+        <h2>Contact Me</h2>
+        <hr style="width:200px" class="w3-opacity">
+        <br>
+        <h3><strong style="color: #e8b900;">USource Global Services (Philippines)</strong></h3>
+        <p style="margin-bottom: 0px;">MVL Centre, 6th Floor, National Road, Lipa, 4217</p>
+        <p>mark.m@usource.me</p>
+        <br>
+      </section>
+    </div>
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+
+<!-- Zoom Image JS -->
+<script type = "text/javascript">
+$(document).ready(function() {
+  $('.zoom-image').hover(function() {
+    $(this).siblings('.zoom-image').css('z-index', 10);
+    $(this).css('z-index', 99);
+  })
+})
+</script>
+
+
+
+<script type = "text/javascript">
+var current = 1; //keeps track of the current div
+var height = $('.roles').height(); //the height of the roles div
+var numberDivs = $('.roles').children().length; //the number of children of the roles div
+var first = $('.roles div:nth-child(1)'); //the first div nested in roles div
+setInterval(function() {
+  var number = current * -height;
+  first.css('margin-top', number + 'px');
+  if (current === numberDivs) {
+      first.css('margin-top', '0px');
+      current = 1;
+  } else current++;
+}, 2000);
+</script>
+
+
+<script type = "text/javascript">
+jQuery(document).ready(function(){
+  //Initialize animation
+  var animation = 0;
+
+  jQuery(window).scroll(function(){
+    //Get position of the scrollbar
+    var pos = $(window).scrollTop();
+
+    //Get position of the #skills element
+    var skills = $('#skills').offset().top;
+    skills = skills - 200;
+
+    //Check if user has scrolled to the #skills element and if the animation has not been fired yet.
+    if(pos > skills && animation ==  0 ){
+      animation = 1;
+      jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },2500);
+      });
+    }
+  });
+});
+</script>
+    
+  </body>
+</html>
